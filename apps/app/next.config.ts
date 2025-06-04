5cd51c15efba893284e7be5a255e0ec6edba748a
Ayush <spanhornet@pm.me>
2025-06-04 15:38:06 -0500

    feat(app): configure domains for image handling

--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "standalone",
+  images: {
+    domains: ["pub-e4c1566b96d64455a14b284ceccd2ba7.r2.dev"],
+  },
 };
 
 export default nextConfig;
